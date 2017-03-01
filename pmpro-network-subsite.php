b50c9efe3ac849bb094e5a641ddc4f56bb854834
Jason Coleman <jason@strangerstudios.com>
2017-03-01 07:33:40 -0500

    Improving readme documentation and updating add on name.

--- pmpro-network-subsite.php
+++ pmpro-network-subsite.php
@@ -1,19 +1,16 @@
 <?php
 /*
-Plugin Name: Paid Memberships Pro Network Subsite Helper
+Plugin Name: Paid Memberships Pro - Multisite Membership Add On
 Plugin URI: http://www.paidmembershipspro.com/add-ons/pmpro-network-subsites/
-Description: Replacement for Paid Memberships Pro meant to be run on a network site, pointing to another network site's PMPro install for membership checks/etc.
-Version: .4
+Description: Manage memberships at the network’s main site (the primary domain of the network) and provide/restrict access on subsites in the network.
+Version: .4.1
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
-/*	
-	Copyright 2011	Stranger Studios	(email : jason@strangerstudios.com)	 
-	This code is licensed under the GPLv2.
-*/
 
-if(!defined('PMPRO_NETWORK_MAIN_DB_PREFIX'))
-	define('PMPRO_NETWORK_MAIN_DB_PREFIX', 'wp');
+if( ! defined( 'PMPRO_NETWORK_MAIN_DB_PREFIX' ) ) {
+	define( 'PMPRO_NETWORK_MAIN_DB_PREFIX', 'wp' );
+}
 
 /*
 	Make sure this plugin loads after Paid Memberships Pro
@@ -21,10 +18,10 @@ if(!defined('PMPRO_NETWORK_MAIN_DB_PREFIX'))
 function pmpron_subsite_activated_plugin() {
 	// ensure path to this file is via main wp plugin path
 	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
-	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
+	$this_plugin = plugin_basename( trim( $wp_path_to_this_file ) );
 	
 	//load plugins
-	$active_plugins = get_option('active_plugins');
+	$active_plugins = get_option( 'active_plugins' );
 	
 	//where am I?
 	$this_plugin_key = array_search($this_plugin, $active_plugins);
@@ -34,9 +31,9 @@ function pmpron_subsite_activated_plugin() {
 	$active_plugins[] = $this_plugin;
 	
 	//update option
-	update_option('active_plugins', $active_plugins);	
+	update_option( 'active_plugins', $active_plugins );
 }
-add_action("activated_plugin", "pmpron_subsite_activated_plugin");
+add_action( 'activated_plugin', 'pmpron_subsite_activated_plugin' );
 
 /*
 	Now update wpdb tables.
@@ -44,28 +41,28 @@ add_action("activated_plugin", "pmpron_subsite_activated_plugin");
 	(Updated again in init to get all cases.)
 */
 global $wpdb;
-$wpdb->pmpro_memberships_users = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_memberships_users";
-$wpdb->pmpro_membership_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_membership_levels";
-$wpdb->pmpro_membership_levelmeta = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_membership_levelmeta";
-$wpdb->pmpro_membership_orders = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_membership_orders";
-$wpdb->pmpro_discount_codes = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_discount_codes";
-$wpdb->pmpro_discount_codes_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_discount_codes_levels";
-$wpdb->pmpro_discount_codes_uses = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_discount_codes_uses";
+$wpdb->pmpro_memberships_users = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_memberships_users';
+$wpdb->pmpro_membership_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_membership_levels';
+$wpdb->pmpro_membership_levelmeta = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_membership_levelmeta';
+$wpdb->pmpro_membership_orders = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_membership_orders';
+$wpdb->pmpro_discount_codes = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_discount_codes';
+$wpdb->pmpro_discount_codes_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_discount_codes_levels';
+$wpdb->pmpro_discount_codes_uses = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_discount_codes_uses';
 
 //get levels again
 function pmpron_init_get_levels() {
 	global $wpdb, $membership_levels;
-	$membership_levels = $wpdb->get_results( "SELECT * FROM {$wpdb->pmpro_membership_levels}", OBJECT );
+	$membership_levels = $wpdb->get_results( 'SELECT * FROM {$wpdb->pmpro_membership_levels}', OBJECT );
 }
-add_action('init', 'pmpron_init_get_levels', 1);
+add_action( 'init', 'pmpron_init_get_levels', 1 );
 
 /*
 	Hide admin stuff
 */
 function pmpron_subsite_init() {
 	//remove admin pages
-	remove_action('admin_menu', 'pmpro_add_pages');
-	remove_action('admin_bar_menu', 'pmpro_admin_bar_menu');
+	remove_action( 'admin_menu', 'pmpro_add_pages' );
+	remove_action( 'admin_bar_menu', 'pmpro_admin_bar_menu' );
 	
 	//remove membership level from edit users page
 	remove_action( 'show_user_profile', 'pmpro_membership_level_profile_fields' );
@@ -74,12 +71,28 @@ function pmpron_subsite_init() {
 	
 	//update wpdb tables again
 	global $wpdb;
-	$wpdb->pmpro_memberships_users = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_memberships_users";
-	$wpdb->pmpro_membership_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_membership_levels";
-	$wpdb->pmpro_membership_levelmeta = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_membership_levelmeta";
-	$wpdb->pmpro_membership_orders = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_membership_orders";
-	$wpdb->pmpro_discount_codes = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_discount_codes";
-	$wpdb->pmpro_discount_codes_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_discount_codes_levels";
-	$wpdb->pmpro_discount_codes_uses = PMPRO_NETWORK_MAIN_DB_PREFIX . "_pmpro_discount_codes_uses";
+	$wpdb->pmpro_memberships_users = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_memberships_users';
+	$wpdb->pmpro_membership_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_membership_levels';
+	$wpdb->pmpro_membership_levelmeta = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_membership_levelmeta';
+	$wpdb->pmpro_membership_orders = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_membership_orders';
+	$wpdb->pmpro_discount_codes = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_discount_codes';
+	$wpdb->pmpro_discount_codes_levels = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_discount_codes_levels';
+	$wpdb->pmpro_discount_codes_uses = PMPRO_NETWORK_MAIN_DB_PREFIX . '_pmpro_discount_codes_uses';
 }
-add_action("init", "pmpron_subsite_init", 15);
+add_action( 'init', 'pmpron_subsite_init', 15 );
+
+/*
+	Function to add links to the plugin row meta
+*/
+function pmpron_subsite_plugin_row_meta($links, $file) {
+	if(strpos($file, 'pmpro-network-subsite.php') !== false)
+	{
+		$new_links = array(
+			'<a href="' . esc_url('https://www.paidmembershipspro.com/add-ons/plus-add-ons/pmpro-network-membership/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpron_subsite' ) ) . '">' . __( 'Docs', 'pmpron_subsite' ) . '</a>',
+			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpron_subsite' ) ) . '">' . __( 'Support', 'pmpron_subsite' ) . '</a>',
+		);
+		$links = array_merge($links, $new_links);
+	}
+	return $links;
+}
+add_filter('plugin_row_meta', 'pmpron_subsite_plugin_row_meta', 10, 2);
--- readme.txt
+++ readme.txt
@@ -1,27 +1,28 @@
-=== Paid Memberships Pro Network Subsite Helper ===
+=== Paid Memberships Pro - Multisite Membership Add On ===
 Contributors: strangerstudios
-Tags: paid memberships pro, pmpro, network sites, wpmu
+Tags: paid memberships pro, pmpro, network, network membership, multisite, wpmu
 Requires at least: 3.0
 Tested up to: 3.6.1
-Stable tag: .4
+Stable tag: .4.1
 
-For sites running WordPress Multsite, allows you to manage memberships at the “Main” Network site and provide/restrict access on other Network Subsites.
+Manage memberships at the network's main site (the primary domain of the network) and provide/restrict access on subsites in the network.
 
 == Description ==
 
-With the Paid Memberships Pro plugin and this plugin activated, new users will be able to choose a site name and title at checkout. A site will be created for them after registering. If they cancel their membership or have it removed, the site will be deactivated. If they sign up for a membership again, the site will be reactivated.
+This add on allows you to sell memberships at the main site that provide access to members-only content on a site or sites throughout the entire multisite network.
+
+All of the membership levels and users are managed on the main network site. The subsites look to the main network site's database to mirror the membership levels available and to check a user's access.
 
 == Installation ==
 
-1. DO NOT “network activate” Paid Memberships Pro, PMPro Network Subsite, or any PMPro add ons. You should always activate the plugin on the individual site.
-1. Make sure you have properly configured Network Sites on your WP install.
-1. Make sure you have the Paid Memberships Pro plugin installed, activated, and setup on the main site where users will checkout for membership.
-1. Upload the ‘pmpro-network-subsite’ directory to the ‘/wp-content/plugins/’ directory of your site.
-1. Edit the PMPRO_NETWORK_MAIN_DB_PREFIX definition at the top of pmpro-network-subsite-helper.php if your main site uses a different DB prefix.
-1. DO NOT activate the pmpro-network-subsite plugin on the “Main” site (i.e. where people checkout) of your network.
-1. DO Activate the plugin through the ‘Plugins’ menu in WordPress on each subsite that should mirror memberships for the main site.
-1. Paid Memberships Pro must be active and configured on the “Main” Network site.
-1. Paid Memberships Pro must be activate but does not need to be configured on the other Network Subsites.
+1. Install Paid Memberships Pro via Network Admin > Plugins > Add New. Do not "Network Activate" the plugin.
+1. On your main network site, activate and configure Paid Memberships Pro by following the Initial Plugin Setup guide.
+1. Upload the 'pmpro-network-subsite' directory to the 'wp-content/plugins/' directory of your WordPress Multisite environment.
+1. For each site in your network that has member content, navigate to the site's Dashboard > Plugins. Activate Paid Memberships Pro AND the Multisite Membership Add On.
+1. DO NOT activate the 'pmpro-network-subsite' plugin on the “Main” site (i.e. where people checkout) of your network.
+1. Make sure that the constant PMPRO_NETWORK_MAIN_DB_PREFIX is properly defined for your main network site in your wp-config.php file. For example: define('PMPRO_NETWORK_MAIN_DB_PREFIX', 'wp');
+
+You will now be able to create members-only content on subsites in the network. The membership levels of your main site are mirrored in each subsite.
 
 == Frequently Asked Questions ==
 
@@ -34,6 +35,9 @@ Please post it in the GitHub issue tracker here: https://github.com/strangerstud
 Please visit our premium support site at http://www.paidmembershipspro.com for more documentation and our support forums.
 
 == Changelog ==
+= .4.1 =
+* ENHANCEMENT: Improving readme documentation and updating add on name.
+
 = .4 =
 * ENHANCEMENT: Checks first if PMPRO_NETWORK_MAIN_DB_PREFIX is already defined before defaulting to wp. This allows you to set the constant in your wp-config.php instead of in the plugin file.
 * ENHANCEMENT: Now also forwarding the _pmpro_discount_codes, _pmpro_discount_codes_levels, _pmpro_discount_codes_uses, and _pmpro_membership_levelmeta tables.
